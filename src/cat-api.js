6f390fd9368925e68d78f2d365e561e66561647b
Dondik Artem <108674705+Impartia1@users.noreply.github.com>
2023-06-11 20:26:36 +0300

    second try

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,60 +1,18 @@
-function handleRequest(url) {
-  const breedSelectElement = document.querySelector('select.breed-select');
-  const loaderElement = document.querySelector('p.loader');
-  const errorElement = document.querySelector('p.error');
+const APIKEY = 'live_ZwtdVVmq75qDAnNdIkPEL2JRy2O3vpU1TfgoghmTQxNEfWyXdWmUQ7dJ7UUpUHIc'
 
-  breedSelectElement.style.display = 'none';
-  loaderElement.style.display = 'block';
-  errorElement.style.display = 'none';
 
-  return fetch(url, {
-      headers: {
-        'x-api-key': 'live_ZwtdVVmq75qDAnNdIkPEL2JRy2O3vpU1TfgoghmTQxNEfWyXdWmUQ7dJ7UUpUHIc' // Замініть 'YOUR_API_KEY_HERE' на свій ключ API
-      }
-    })
-    .then(response => {
-      loaderElement.style.display = 'none';
+function fetchBreeds() {
+  return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${APIKEY}`).then(response => {
+    return response.json();
+  })
 
-      if (!response.ok) {
-        throw new Error('Network response was not OK');
-      }
-
-      return response.json();
-    })
-    .catch(error => {
-      breedSelectElement.style.display = 'none';
-      loaderElement.style.display = 'none';
-      errorElement.style.display = 'block';
-      console.error('Request error:', error);
-      throw error;
-    });
 }
 
-// Функція для отримання списку порід
-export function fetchBreeds() {
-  const breedSelectElement = document.querySelector('select.breed-select');
-
-  breedSelectElement.style.display = 'none';
-
-  const url = 'https://api.thecatapi.com/v1/breeds';
-
-  return handleRequest(url)
-    .then(breeds => {
-      breedSelectElement.innerHTML = breeds.map(breed => {
-        return `<option value="${breed.id}">${breed.name}</option>`;
-      }).join('');
-
-      breedSelectElement.style.display = 'block';
-
-      return breeds;
-    });
+function fetchCatByBreed(breedId) {
+  return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${APIKEY}`).then(response => {
+    return response.json();
+  })
 }
 
 
-
-
-export function fetchCatByBreed(breedId) {
-  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-
-  return handleRequest(url);
-}
+export { fetchBreeds, fetchCatByBreed }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
-
-
 // Функція для заповнення селекту зі списком порід
 function populateBreedSelect(breeds) {
   const breedSelect = document.querySelector('.breed-select');
@@ -44,12 +42,27 @@ function showCatInfo(cat) {
 
 // Отримання списку порід при завантаженні сторінки
 document.addEventListener('DOMContentLoaded', () => {
+  const breedSelect = document.querySelector('select.breed-select');
+  const loaderElement = document.querySelector('p.loader');
+  const errorElement = document.querySelector('p.error');
+
+  breedSelect.style.display = 'none';
+  loaderElement.style.display = 'block';
+  loaderElement.textContent = 'Loading data, please wait...';
+  errorElement.style.display = 'none';
+
   fetchBreeds()
     .then(breeds => {
       populateBreedSelect(breeds);
+
+      breedSelect.style.display = 'block';
+      loaderElement.style.display = 'none';
     })
     .catch(error => {
       console.error('Error fetching breeds:', error);
+      errorElement.style.display = 'block';
+      errorElement.textContent = 'Error loading data.';
+      loaderElement.style.display = 'none';
     });
 });
 
@@ -57,119 +70,27 @@ document.addEventListener('DOMContentLoaded', () => {
 const breedSelect = document.querySelector('select.breed-select');
 breedSelect.addEventListener('change', () => {
   const selectedBreedId = breedSelect.value;
+  const loaderElement = document.querySelector('p.loader');
+  const errorElement = document.querySelector('p.error');
+  const catInfoDiv = document.querySelector('.cat-info');
+
+  loaderElement.style.display = 'block';
+  loaderElement.textContent = 'Loading cat information...';
+  errorElement.style.display = 'none';
+  catInfoDiv.innerHTML = '';
 
   fetchCatByBreed(selectedBreedId)
     .then(cat => {
       showCatInfo(cat);
+      loaderElement.style.display = 'none';
     })
     .catch(error => {
       console.error('Error fetching cat:', error);
+      errorElement.style.display = 'block';
+      errorElement.textContent = 'Error loading cat information.';
+      loaderElement.style.display = 'none';
     });
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const url = `https://api.thecatapi.com/v1/breeds`;
-// const api_key ='live_ZwtdVVmq75qDAnNdIkPEL2JRy2O3vpU1TfgoghmTQxNEfWyXdWmUQ7dJ7UUpUHIc';
-// let storedBreeds = [];
-
-// fetch(url, {headers: {
-//   'x-api-key': api_key,
-//   },
-// })
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(data => {
-//           //filter to only include those with an `image` object
-//     data = data.filter(img => img.image?.url != null);
-
-//     storedBreeds = data;
-
-//     for (let i = 0; i < storedBreeds.length; i++) {
-//       const breed = storedBreeds[i];
-//       let option = document.createElement('option');
-
-//             //skip any breeds that don't have an image
-//       if (!breed.image) continue;
-
-//             //use the current array index
-//       option.value = i;
-//       option.innerHTML = `${breed.name}`;
-//       document.querySelector('.breed-select').appendChild(option);
-//     }
-      
-//           //    show the first breed by default
-//           // showBreedImage(0);
-//           // showBreedImage(10);
-//   })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-//   function showBreedImage(index) {
-//     const divRef = document.querySelector('.cat-info');
-//     const imgElement = divRef.querySelector('img');
-
-//     if (imgElement) {
-//       imgElement.src = storedBreeds[index].image.url;
-//     }
-//     else {
-//       let imgRef = document.createElement('img');
-
-//       document.querySelector('.cat-info').appendChild(imgRef);
-//       imgRef.src = storedBreeds[index].image.url;
-//     }
-
-//     document.getElementById('temperament').textContent = storedBreeds[index].temperament;
-
-//     document.getElementById('decsription').textContent = storedBreeds[index].description;
-//   }
-//   document.querySelector('.breed-select').addEventListener('change', () => {
-//     const selectedBreedId = document.querySelector('.breed-select').value;
-
-//     fetchCatByBreed(selectedBreedId)
-//     .then(cat => {
-//       showBreedImage(cat);
-//     })
-//     .catch(error => {
-//       console.error('Error fetching cat:', error);
-//     });
-//   });
